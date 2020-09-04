feb18387e8ca9083ce4ddf6f835bdd26f50761cb
dagapp <66718169+dagapp@users.noreply.github.com>
2020-09-04 22:02:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+//
+//Example main code
+//
+
 #include <iostream>
 #include "QuadraticEqSolver.hpp"
 
@@ -5,11 +9,14 @@ using namespace std;
 
 int main()
 {
+    //Creating an equation object
     QuadraticEq equation(0, 0, -9);
     
+    //equation.SolutionCount contains a number of solutions equation has
     cout << "Solution count: " << equation.SolutionCount << endl;
     for (int i = 0; i < equation.SolutionCount; i++)
     {
+        //equation.Solutions is an array of solutions (NULL, 1 element or 2 elements)
         cout << "Solution " << i + 1 << ": " << equation.Solutions[i] << endl;
     }
     
