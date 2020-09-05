1800c48b114e63e92797b565f30f68fff07de89d
dagapp <66718169+dagapp@users.noreply.github.com>
2020-09-05 06:36:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,14 +9,20 @@ using namespace std;
 
 int main()
 {
+    //
     //Creating an equation object
+    //
     QuadraticEq equation(4, 7, 3);
     
+    //
     //equation.SolutionCount contains a number of solutions equation has
+    //
     cout << "Solution count: " << equation.SolutionCount << endl;
     for (int i = 0; i < equation.SolutionCount; i++)
     {
+        //
         //equation.Solutions is an array of solutions (NULL, 1 element or 2 elements)
+        //
         cout << "Solution " << i + 1 << ": " << equation.Solutions[i] << endl;
     }
     
